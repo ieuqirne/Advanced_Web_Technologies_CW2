99737079d91f9c9f91c4c9a92386eb42dd839e10
EnriqueBC <belenguercarrascoenrique@gmail.com>
2018-11-09 23:42:36 +0000

    Templates SetUp

--- SourceCode/app.py
+++ SourceCode/app.py
@@ -1,6 +1,14 @@
-from flask import Flask 
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def  index():
-	return 'Hello World'
+	return render_template('base.html'),200
+
+@app.route('/user/')
+def  user():
+	return render_template('user.html'),200
+
+
+if __name__==" __main__ ":
+	app.run(host ='0.0.0.0',debug=True)
