3d02f11977a9ef8e77f5d33722c0838d097d3856
EnriqueBC <belenguercarrascoenrique@gmail.com>
2018-12-01 15:07:26 +0000

    Chapter 9 Role Verification Done

--- SourceCode/app/main/__init__.py
+++ SourceCode/app/main/__init__.py
@@ -4,3 +4,9 @@ from flask import Blueprint
 main = Blueprint('main', __name__)
 
 from . import views, errors
+from ..models import Permission
+
+#Checking Permissions
+@main.app_context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
--- SourceCode/run.bat
+++ SourceCode/run.bat
@@ -4,6 +4,9 @@ call activate
 cd../..
 cd sourcecode
 pip install -r requirements.txt
+set MAIL_USERNAME=belenguercarrascoenrique@gmail.com
+set MAIL_PASSWORD=aaaaa
+set FLASKY_ADMIN=belenguercarrascoenrique@gmail.com
 set FLASK_APP=flasky.py
 set FLASK_ENV=development
 python -m flask run --host=0.0.0.0 --port 9110
